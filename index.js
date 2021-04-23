838c182461b46dec40d9039388c7556b960068c4
crzenna <40445873@live.napier.ac.uk>
2021-04-23 10:02:10 +0200

    commit

---
+++ index.js
@@ -0,0 +1,307 @@
+function functionA(){
+
+		var button = document.getElementById("breakfasthome");
+        window.location = 'breakfast_home.html';
+    };
+
+function functionB(){
+
+		var button = document.getElementById("breakfastout");
+        window.location = 'breakfast_out.html';
+	};
+
+function functionC(){
+
+		var button = document.getElementById("goback");
+        window.location = 'play.html';
+	};
+
+function functionD(){
+
+		var button = document.getElementById("bikeride");
+        window.location = 'bike_ride.html';
+	};
+
+function functionE(){
+
+		var button = document.getElementById("beachwalk");
+        window.location = 'beach_walk.html';
+	};
+
+function functionF(){
+
+		var button = document.getElementById("beachside");
+        window.location = 'beach_side.html';
+	};
+
+function functionG(){
+
+		var button = document.getElementById("brotherpickup");
+        window.location = 'brother_pickup.html';
+	};
+
+function functionH(){
+
+		var button = document.getElementById("gobackA");
+        window.location = 'breakfast_home.html';
+	};
+
+function functionI(){
+
+		var button = document.getElementById("sanpietro");
+        window.location = 'sanpietro.html';
+	};
+
+function functionJ(){
+
+		var button = document.getElementById("caroman");
+        window.location = 'caroman.html';
+	};
+
+function functionK(){
+
+		var button = document.getElementById("restaurantlunch");
+        window.location = 'restaurant_lunch.html';
+	};
+
+function functionL(){
+
+		var button = document.getElementById("manuelunch");
+        window.location = 'manuel_lunch.html';
+	};
+
+function functionM(){
+
+		var button = document.getElementById("gobackB");
+        window.location = 'breakfast_out.html';
+	};
+
+function functionN(){
+
+		var button = document.getElementById("accept");
+        window.location = 'accept_invitation.html';
+	};
+
+function functionO(){
+
+		var button = document.getElementById("decline");
+        window.location = 'decline_invitation.html';
+	};
+
+function functionP(){
+
+		var button = document.getElementById("torestaurant");
+        window.location = 'go_restaurant.html';
+	};
+
+function functionQ(){
+
+		var button = document.getElementById("gohome");
+        window.location = 'go_home.html';
+	};
+
+function functionR(){
+
+		var button = document.getElementById("gobackC");
+        window.location = 'bike_ride.html';
+	};
+
+function functionS(){
+
+		var button = document.getElementById("caterina");
+        window.location = 'caterina.html';
+	};
+
+function functionT(){
+
+		var button = document.getElementById("eatsandro");
+        window.location = 'eat_sandro.html';
+	};
+
+function functionU(){
+
+		var button = document.getElementById("gobackD");
+        window.location = 'sanpietro.html';
+	};
+
+function functionV(){
+
+		var button = document.getElementById("final");
+        window.location = 'final.html';
+	};
+
+function functionW(){
+
+		var button = document.getElementById("visitpellestrina");
+        window.location = 'www.pellestrina.com';
+	};
+
+function functionX(){
+
+		var button = document.getElementById("boat");
+        window.location = 'boat.html';
+	};
+
+function functionY(){
+
+		var button = document.getElementById("pizzabeach");
+        window.location = 'pizza_beach.html';
+	};
+
+function functionZ(){
+
+		var button = document.getElementById("pizzabeach");
+        window.location = 'pizza_beach.html';
+	};
+
+function functionAA(){
+
+		var button = document.getElementById("gobackE");
+        window.location = 'caroman.html';
+	};
+
+function functionAB(){
+
+		var button = document.getElementById("gobackF");
+        window.location = 'beach_walk.html';
+	};
+
+function functionAC(){
+
+		var button = document.getElementById("cousins");
+        window.location = 'cousins.html';
+	};
+
+function functionAD(){
+
+		var button = document.getElementById("sottomarina");
+        window.location = 'sottomarina.html';
+	};
+
+function functionAE(){
+
+		var button = document.getElementById("gobackG");
+        window.location = 'restaurant_lunch.html';
+	};
+
+function functionAF(){
+
+		var button = document.getElementById("afternooncaroman");
+        window.location = 'afternoon_caroman.html';
+	};
+
+function functionAG(){
+
+		var button = document.getElementById("afternoonbeach");
+        window.location = 'afternoon_beach.html';
+	};
+
+function functionAH(){
+
+		var button = document.getElementById("gobackH");
+        window.location = 'manuel_lunch.html';
+	};
+
+function functionAJ(){
+
+		var button = document.getElementById("gobackI");
+        window.location = 'beach_side.html';
+	};
+
+function functionAK(){
+
+		var button = document.getElementById("lidobeach");
+        window.location = 'lido_beach.html';
+	};
+
+function functionAL(){
+
+		var button = document.getElementById("venice");
+        window.location = 'venice.html';
+	};
+
+function functionAM(){
+
+		var button = document.getElementById("lidoeat");
+        window.location = 'lido_eat.html';
+	};
+
+function functionAN(){
+
+		var button = document.getElementById("irene");
+        window.location = 'irene.html';
+	};
+
+function functionAO(){
+
+		var button = document.getElementById("gobackJ");
+        window.location = 'accept_invitation.html';
+	};
+
+function functionAP(){
+
+		var button = document.getElementById("gobackK");
+        window.location = 'decline_invitation.html';
+	};
+
+function functionAQ(){
+
+		var button = document.getElementById("gobackL");
+        window.location = 'brother_pickup.html';
+	};
+
+function functionAR(){
+
+		var button = document.getElementById("restauranteat");
+        window.location = 'restaurant_eat.html';
+	};
+
+function functionAS(){
+
+		var button = document.getElementById("restauranthelp");
+        window.location = 'restaurant_help.html';
+	};
+
+function functionAT(){
+
+		var button = document.getElementById("eliana");
+        window.location = 'eliana.html';
+	};
+
+function functionAU(){
+
+		var button = document.getElementById("sara");
+        window.location = 'sara.html';
+	};
+
+function functionAV(){
+
+		var button = document.getElementById("gobackM");
+        window.location = 'go_restaurant.html';
+	};
+
+function functionAW(){
+
+		var button = document.getElementById("gobackN");
+        window.location = 'go_home.html';
+	};
+
+function functionAY(){
+
+		var button = document.getElementById("playagain");
+        window.location = 'play.html';
+	};
+
+function functionAlert(){
+	alert('Hello. I am sorry - this form is not working yet. You can reach me at 40445873@live.napier.ac.uk')
+	};
+
+	
+	
+
+
+
+
+    
+
+    
